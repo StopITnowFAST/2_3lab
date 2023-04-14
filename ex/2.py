2e25f4eecbd7dd06d0195433f56bfb1e51eea207
StopITnowFAST <Bertasdraf@mail.ru>
2023-04-14 13:54:37 +0300

    изменил до конца

--- ex/2.py
+++ ex/2.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import json
 
-# вариант 1
-with open("foo.txt", "r") as f:
-    contents = f.read()
-my_list = json.loads(contents)
+if __name__ == "__main__":
+    # вариант 1
+    with open("foo.txt", "r") as f:
+        contents = f.read()
+    my_list = json.loads(contents)
 
-# вариант 2
-with open("foo.txt", "r") as f:
-    my_list = json.load(f)
+    # вариант 2
+    with open("foo.txt", "r") as f:
+        my_list = json.load(f)
